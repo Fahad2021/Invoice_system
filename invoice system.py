05b37da4204105af3a6749e91a2e664b1f4af831
Fahad <fahadrahman810@gmail.com>
2021-03-21 23:44:46 +0600

    create customers details

--- invoice system.py
+++ invoice system.py
@@ -44,7 +44,21 @@ class Bill_App:
         self.c_name = StringVar()
         self.c_phone = StringVar()
         self.bill_no = StringVar()
-        
+
+        # ..................customer details frame
+        F1 = LabelFrame(self.root, bd=10, relief=GROOVE, text="Customer Details", font=("times new roman", 15, "bold"),fg="white", bg=bg_color)
+        F1.place(x=0, y=80, relwidth=1)
+        cname_lbl = Label(F1, text="Customer Name", bg=bg_color, fg=fg, font=("times new roman", 18, "bold")).grid(row=0, column=0, padx=20, pady=5)
+        cname_txt = Entry(F1, width=15, font="arial 15", textvariable=self.c_name, bd=7, relief=SUNKEN).grid(row=0,column=1,pady=5,padx=10)
+
+        cphn_lbl = Label(F1, text="Phone No.", bg=bg_color, fg=fg, font=("times new roman", 18, "bold")).grid(row=0,column=2,padx=20,pady=5)
+        cpn_txt = Entry(F1, width=15, font="arial 15", textvariable=self.c_phone, bd=7, relief=SUNKEN).grid(row=0,column=3,pady=5,padx=10)
+
+        c_bill_lbl = Label(F1, text="Bill Number", bg=bg_color, fg=fg, font=("times new roman", 18, "bold")).grid(row=0,column=4,padx=20,pady=5)
+        c_bill_txt = Entry(F1, width=15, font="arial 15", textvariable=self, bd=7, relief=SUNKEN).grid(row=0, column=5, pady=5, padx=10)
+
+        bill_btn = Button(F1, text="Search", command=self, width=10, bd=7, font='arial 12 bold').grid(row=0,column=6,padx=10,pady=10)
+
 
 root=Tk()
 ob=Bill_App(root)
